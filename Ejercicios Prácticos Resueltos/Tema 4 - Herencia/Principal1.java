41718d353ec75dc50adc88663cd1b02f5f822a38
Alvar Velázquez de León Lavarrios <yatochbarum99@hotmail.com>
2023-10-23 19:23:15 -0600

    Agregados ejercicios resueltos tema 4 (pendiente)
    Características:
    - Agregados los ejercicios prácticos resueltos para el Tema 4 - Herencia
      (quedaron pendientes los ejercicios que vienen en el código
      Principal2.java)
    - Corrección importante en la presentación de la teoría: en el tema 4 de
      Herencia se corrigieron errores con respecto al uso de la palabra
      reservada "super" y se explicó cómo se debe y cómo no se debe
      utilizar.

---
+++ Ejercicios Prácticos Resueltos/Tema 4 - Herencia/Principal1.java
@@ -0,0 +1,49 @@
+public class Principal1 {
+	
+	public static void main(String[] args){
+
+		// Crea un nuevo objeto de tipo Goomba
+		Goomba goomba = new Goomba();
+
+		/* -------- Ejercicio #1.1 ----------
+		Antes de iniciar este ejercicio, te darás cuenta de que la clase Goomba.java tiene un solo
+		atributo definido. Sin embargo, en las líneas de código que se encuentran comentadas a
+		continuación, están tratando de imprimir el valor de atributos que no vienen definidas en
+		dicha clase.
+
+		Descomenta las líneas de código debajo de este comentario, compila esta clase y ejecútala.
+		En caso de que no de error de compilación y puedas ejecutar este programa, responde:
+		- ¿Qué se imprime en pantalla?
+		- ¿Por qué sucede esto? ...o mejor dicho... ¿qué hay de "trasfondo"?
+		*/
+
+		System.out.println("\n-----------------------");
+		System.out.println("Nombre del Goomba: " + goomba.nombre);
+		System.out.println("Vida del Goomba: " + goomba.vida);
+
+
+
+		/* -------- Ejercicio #1.2 ----------
+		Descomenta la siguiente línea de código y vuelve a compilar esta clase.
+		Cuando intentes volver a compilar verás que te dará un error. Responde,
+		¿por qué sucede esto?
+		*/
+		
+		//System.out.println("Tiempo de reaparición actual del Goomba: " + goomba.tiempo_reaparicion_actual);
+
+
+		/* -------- Ejercicio #1.3 ----------
+		Vuelve a comentar la línea de código anterior.
+
+		Ahora, te darás cuenta que en ambas clases, Enemigo.java y Goomba.java, tienen un atributo
+		llamado "vida_maxima". Debajo de este comentario, escribe las líneas de código necesario 
+		para mostrar en pantalla tanto la vida máxima propia del Goomba como la vida máxima "genérica" 
+		(la de Enemigo.java).
+
+		Compila y ejecuta de nuevo para cerciorarte de tu resultado.
+		*/
+
+		goomba.imprimirVidaMaxima();
+	}
+
+}
